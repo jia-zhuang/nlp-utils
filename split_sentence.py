4ca0ffedc00b2f293ce598d408cdb778d88f876c
Adam <jiazzzz@qq.com>
2020-08-19 09:23:49 +0800

    Update split_sentence.py

--- split_sentence.py
+++ split_sentence.py
@@ -1,11 +1,13 @@
 import re
+import math
 # 将 content_field 的段落拆成句子
 SPLIT_PATTERN = re.compile(r'(。|！|？?!)')
 def split_text_to_sentences(text, max_seq_length=192):
     res = []
     if isinstance(text, str):
         sentences = SPLIT_PATTERN.split(text)
-        for i in range(len(sentences) // 2):
-            sent = sentences[2*i] + sentences[2*i + 1]
-            res.append(sent.strip()[:max_seq_length])
+        for i in range(math.ceil(len(sentences) / 2)):
+            sent = ''.join(sentences[(2*i):(2*i + 2)])
+            if sent:
+                res.append(sent.strip()[:max_seq_length])
     return res
